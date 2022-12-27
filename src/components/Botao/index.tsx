61a43da958a7760356e45ad0897ce270dc42c360
anderson.carvalho <anderson.carvalho@accurate.com.br>
2022-12-27 17:20:27 -0300

    Protótipo não funcional

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,11 @@
+import style from './Botao.module.scss'
+
+const Botao = () => {
+    return (
+        <button className={style.botao}>
+            Botão
+        </button>
+    )
+}
+
+export default Botao
